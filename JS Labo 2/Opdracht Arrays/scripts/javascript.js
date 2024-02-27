d1bc276a3e413cccb8b2ec75e89e35f863e92524
WardCornelis <ward.cornelis@student.vives.be>
2024-02-27 14:47:07 +0100

    Update javascript.js

--- JS Labo 2/Opdracht Arrays/scripts/javascript.js
+++ JS Labo 2/Opdracht Arrays/scripts/javascript.js
@@ -1,18 +1,17 @@
 const setup = () => {
-}
+    let lastNames = ['Deceuninck','Cornelis','Snoeck','Vermeulen','Deteur'];
 
-let lastNames = ['Deceuninck','Cornelis','Snoeck','Vermeulen','Deteur'];
+    console.log(lastNames.length); /** Lengte van de array loggen*/
 
-console.log(lastNames.length); /** Lengte van de array loggen*/
+    console.log(lastNames[0], lastNames[2], lastNames[4]); /** Het eerste, derde en vijfde element loggen*/
 
-console.log(lastNames[0], lastNames[2], lastNames[4]); /** Het eerste, derde en vijfde element loggen*/
+    VoegNaamToe();
 
-VoegNaamToe();
+    console.log(lastNames.join()); /** Uitkomst array weergeven */
 
-console.log(lastNames.join()); /** Uitkomst array weergeven */
-
-function VoegNaamToe() {
-    console.log(lastNames.push(window.prompt("Welke naam wilt u toevoegen en geef deze op","Geef hier de naam op"))); /** Naam opvragen en toevoegen aan de array, resultaat weergeven */
+    const VoegNaamToe = () =>  {
+            console.log(lastNames.push(window.prompt("Welke naam wilt u toevoegen en geef deze op","Geef hier de naam op"))); /** Naam opvragen en toevoegen aan de array, resultaat weergeven */
+        }
 }
 
 window.addEventListener("load", setup);
