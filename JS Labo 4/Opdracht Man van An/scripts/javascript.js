44a441cba1711bdbb002e88e59c83eb46e85a8ce
WardCornelis <ward.cornelis@student.vives.be>
2024-03-05 19:00:55 +0100

    jaj

---
+++ JS Labo 4/Opdracht Man van An/scripts/javascript.js
@@ -0,0 +1,16 @@
+const setup = () => {
+    let tekst = "De man van An geeft geen hand aan ambetante verwanten";
+    let stopZoektocht = false;
+    let startWaarde = 0;
+    let aantal = 0;
+    while(!stopZoektocht) {
+        if (tekst.indexOf("an",startWaarde) !== -1){
+            aantal++;
+            startWaarde = tekst.indexOf("an",startWaarde) + 1;
+        } else {
+            stopZoektocht = true;
+        }
+    }
+    console.log(aantal);
+}
+window.addEventListener("load", setup);
