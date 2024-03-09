59c2b13fb2ee9eeebcdc40612349c47d934087e8
WardCornelis <ward.cornelis@student.vives.be>
2024-03-09 01:27:15 +0100

    Update javascript.js

--- JS Labo 4/Opdracht Man van An/scripts/javascript.js
+++ JS Labo 4/Opdracht Man van An/scripts/javascript.js
@@ -1,16 +1,31 @@
 const setup = () => {
     let tekst = "De man van An geeft geen hand aan ambetante verwanten".toLowerCase();
-    let stopZoektocht = false;
-    let startWaarde = 0;
-    let aantal = 0;
-    while(!stopZoektocht) {
-        if (tekst.indexOf("an",startWaarde) !== -1){
-            aantal++;
-            startWaarde = tekst.indexOf("an",startWaarde) + 1;
+    let stopZoektochtEen = false;
+    let startWaardeEen = 0;
+    let aantalEen = 0;
+
+    while(!stopZoektochtEen) {
+        if (tekst.indexOf("an",startWaardeEen) !== -1){
+            aantalEen++;
+            startWaardeEen = tekst.indexOf("an",startWaardeEen) + 1;
         } else {
-            stopZoektocht = true;
+            stopZoektochtEen = true;
         }
     }
-    console.log(`An komt ${aantal} keer voor in de opgegeven tekst.`);
+
+    let stopZoektochtTwee = false;
+    let aantalTwee = 0;
+    let startWaardeTwee = 1000;
+
+    while(!stopZoektochtTwee) {
+        if (tekst.lastIndexOf("an",startWaardeTwee) !== -1){
+            aantalTwee++;
+            startWaardeTwee = tekst.lastIndexOf("an",startWaardeTwee) -1;
+        } else {
+            stopZoektochtTwee = true;
+        }
+    }
+    console.log(`An komt ${aantalEen} keer voor in de opgegeven tekst. Hierbij is gebruik gemaakt van indexOf.`);
+    console.log(`An komt ${aantalTwee} keer voor in de opgegeven tekst. Hierbij is gebruik gemaakt van lastIndexOf.`);
 }
 window.addEventListener("load", setup);
