a78876c88eb16d1ecff06b31bbd328df8ab42f0a
WardCornelis <ward.cornelis@student.vives.be>
2024-03-21 22:32:10 +0100

    validator

--- JS Labo 6/Extra oefening nodes/Oefening 2/scripts/javascript.js
+++ JS Labo 6/Extra oefening nodes/Oefening 2/scripts/javascript.js
@@ -1,12 +1,12 @@
 const setup = () => {
-    let section = document.querySelector('section:nth-child(2)');
-    let liElementen = section.querySelectorAll('li');
+    let div = document.querySelector('div');
+    let liElementen = div.querySelectorAll('li');
     for(let i = 0; i < liElementen.length; i++) {
         liElementen[i].classList.add("listitem");
     }
     let img = document.createElement("img");
     img.setAttribute('src','Images/WardCornelis.formated.jpg');
     img.setAttribute('alt','Afbeelding van Ward');
-    section.append(img);
+    div.append(img);
 }
 window.addEventListener("load", setup);
