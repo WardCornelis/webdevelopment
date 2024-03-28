ba97dd4b6d2f78611039cc96f22cec149fa89440
WardCornelis <ward.cornelis@student.vives.be>
2024-03-28 15:51:02 +0100

    colorpicker pudate

--- JS Labo 6/Opdracht ColorPicker/scripts/javascript.js
+++ JS Labo 6/Opdracht ColorPicker/scripts/javascript.js
@@ -3,45 +3,42 @@ const setup = () => {
     let inputsSection = document.querySelector('#Inputs');
     inputsSection.addEventListener('change', changeColor);
     inputsSection.addEventListener('input', changeColor);
-    inputsSection.addEventListener('click', addToSaved);
+
+    let saveBtn = document.querySelector('#Savebtn');
+    saveBtn.addEventListener('click', addToSaved);
 
     let savedSection = document.querySelector('#Saved');
     savedSection.addEventListener('click', setToColor);
-    savedSection.addEventListener('click', removeSaved);
 }
 
 const removeSaved = (event) => {
-    if(event.target.matches('.Deletebtn')){
-        let savedSection = document.querySelector('#Saved');
-        savedSection.removeChild(event.target.parentElement);
-    }
+    let savedSection = document.querySelector('#Saved');
+    savedSection.removeChild(event.target.parentElement);
+    event.stopPropagation();
 }
 
 const setToColor = (event) => {
-    if(event.target.matches('.savedColors')){
-        let computedStyle = window.getComputedStyle(event.target).backgroundColor.toString();
-        let redSlider = document.querySelector('#RedSlider');
-        let greenSlider = document.querySelector('#GreenSlider');
-        let blueSlider = document.querySelector('#BlueSlider');
+    let computedStyle = window.getComputedStyle(event.target).backgroundColor.toString();
+    let redSlider = document.querySelector('#RedSlider');
+    let greenSlider = document.querySelector('#GreenSlider');
+    let blueSlider = document.querySelector('#BlueSlider');
 
-        let nakedValues = computedStyle.slice(4,-1);
-        let comma = nakedValues.indexOf(',');
-        let red = nakedValues.slice(0,comma).trim();
-        nakedValues = nakedValues.slice(comma + 1);
-        comma = nakedValues.indexOf(',');
-        let green = nakedValues.slice(0,comma).trim();
-        let blue = nakedValues.slice(comma + 1).trim();
+    let nakedValues = computedStyle.slice(4,-1);
+    let comma = nakedValues.indexOf(',');
+    let red = nakedValues.slice(0,comma).trim();
+    nakedValues = nakedValues.slice(comma + 1);
+    comma = nakedValues.indexOf(',');
+    let green = nakedValues.slice(0,comma).trim();
+    let blue = nakedValues.slice(comma + 1).trim();
 
-        redSlider.value = red;
-        greenSlider.value = green;
-        blueSlider.value = blue;
+    redSlider.value = red;
+    greenSlider.value = green;
+    blueSlider.value = blue;
 
-        changeColor();
-    }
+    changeColor();
 }
 
-const addToSaved = (event) =>{
-    if(event.target.matches('#Savebtn')) {
+const addToSaved = () =>{
         let savedSection = document.querySelector('#Saved');
 
         let red = document.querySelector('#RedSlider').value;
@@ -49,11 +46,19 @@ const addToSaved = (event) =>{
         let blue = document.querySelector('#BlueSlider').value;
 
         let div = document.createElement('div');
+        let input = document.createElement('input');
+        let label = document.createElement('label');
         div.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-        div.innerHTML = `<input class="Deletebtn" type="button" value="X" name="Delete">`;
         div.classList.add('savedColors');
+        label.setAttribute('for', 'Deletebtn');
+        input.setAttribute('id', 'Deletebtn');
+        input.setAttribute('type', 'button');
+        input.setAttribute('value', 'X');
+        input.setAttribute('name', 'Delete');
+        input.addEventListener('click', removeSaved);
+        div.append(label);
+        div.append(input);
         savedSection.append(div);
-    }
 }
 
 const changeColor = () => {
