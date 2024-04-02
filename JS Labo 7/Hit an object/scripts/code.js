d34e34c0ba0fef9e04974929be00603ecfe1020d
WardCornelis <ward.cornelis@student.vives.be>
2024-04-02 16:22:54 +0200

    update / leesbare code

--- JS Labo 7/Hit an object/scripts/code.js
+++ JS Labo 7/Hit an object/scripts/code.js
@@ -3,7 +3,7 @@ let global = {
     IMAGE_SIZE: 48,
     IMAGE_PATH_PREFIX: "images/",
     IMAGE_PATH_SUFFIX: ".png",
-    MOVE_DELAY: 3000,
+    MOVE_DELAY: 1000,
     score: 0,
     start: false,
     timeoutId: 0
@@ -16,37 +16,57 @@ const setup = () => {
     startBtn.addEventListener('click', speelSpel);
 };
 
+//Uitleg van het spel voor het starten
 const toonInfo = () => {
     if(global.start === false) {
         window.alert('Klik op alle afbeeldingen, die geen bom zijn. Doe je dit toch, verlies je!');
     }
 };
 
+//Functie om een spel te spelen
 const speelSpel = () => {
     global.start = true;
+
     let startBtn = document.querySelector('#StartBtn');
     startBtn.remove();
+
     let afbeelding = document.querySelector('#Target');
     afbeelding.addEventListener('click', valideerKlik);
-    setInterval(movePicture, 1000);
+
+    global.timeoutId = setInterval(intervalFuncties, global.MOVE_DELAY);
 };
 
-const valideerKlik = (event) => {
+//Valideren van de aangeklikte afbeelding
+const valideerKlik = () => {
+    let tekst = document.querySelector('#AantalHits');
+
     let afbeelding = document.querySelector('#Target');
-    let actueleSrc = afbeelding.getAttribute('src');
-    if(actueleSrc === "images/0.png") {
+    let src = afbeelding.getAttribute('src');
+    let afbeeldingNummer = parseInt(src.slice(global.IMAGE_PATH_PREFIX.length, -(global.IMAGE_PATH_SUFFIX.length)));
+
+    if(afbeeldingNummer === 0){
         clearInterval(global.timeoutId);
-        if(window.confirm(`Het spel is over, u klikte op een BOM. U heeft een score behaald van ${global.score}! \nWilt u direct opnieuw spelen?`)){
-            speelSpel();
-        } else {
-            location.reload();
-        }
+        global.timeoutId = 0;
+        window.alert('!GAME OVER!\nHerstarten kan door op f5 te drukken');
     } else {
         global.score += 1;
+        tekst.innerText = `Aantal hits ${global.score}`;
         movePicture();
     }
-};
+}
 
+//Combineren de functies die door een interval geactiveerd worden
+const intervalFuncties = () => {
+    logTekst();
+    movePicture();
+}
+
+//Log de huidige score van de speler
+const logTekst = () => {
+    console.log(`Uw heeft momenteel een score van ${global.score}!`);
+}
+
+//Verplaats de afbeelding binnen het speelveld
 const movePicture = () => {
     randomAfbeelding();
     let sprite = document.querySelector('#Target');
@@ -60,13 +80,13 @@ const movePicture = () => {
     sprite.style.top = top+"px";
 };
 
+//Selecteer een afbeelding uit de lijst
 const randomAfbeelding = () => {
     let rand = Math.floor(Math.random() * 5);
     let afbeelding = global.IMAGE_PATH_PREFIX + rand + global.IMAGE_PATH_SUFFIX;
     let sprite = document.querySelector('#Target');
     sprite.setAttribute('src', afbeelding);
 };
-
 
 window.addEventListener("load", setup);
 
