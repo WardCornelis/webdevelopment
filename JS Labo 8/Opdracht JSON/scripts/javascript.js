9e9d37399c56e306bbe62b5a1c66de5d6df7ea9f
WardCornelis <ward.cornelis@student.vives.be>
2024-04-16 16:05:04 +0200

    JSON parsing

--- JS Labo 8/Opdracht JSON/scripts/javascript.js
+++ JS Labo 8/Opdracht JSON/scripts/javascript.js
@@ -1,3 +1,21 @@
 const setup = () => {
+    eersteDeel();
+    tweedeDeel();
+}
+
+const eersteDeel = () => {
+    let student1 = {
+        voornaam: "Eddy",
+        achternaam: "Van Reckevellen",
+        geboortedatum: new Date(1998, 11, 25),
+        studierichting: "Marketing"
+    };
+    let tekst = JSON.stringify(student1);
+    console.log(tekst);
+}
+
+const tweedeDeel = () => {
+    let student2 = JSON.parse('{"voornaam":"Eddy","achternaam":"Van Reckevellen","geboortedatum":"1998-12-24T23:00:00.000Z", "studierichting":"Marketing"}');
+    console.log(student2.voornaam);
 }
 window.addEventListener("load", setup);
