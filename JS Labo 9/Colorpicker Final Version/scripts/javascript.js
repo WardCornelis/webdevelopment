4bc778b319de0ef8216dad9678b646f6907d3b67
WardCornelis <ward.cornelis@student.vives.be>
2024-04-25 16:37:11 +0200

    colorpicker

--- JS Labo 9/Colorpicker Final Version/scripts/javascript.js
+++ JS Labo 9/Colorpicker Final Version/scripts/javascript.js
@@ -1,3 +1,5 @@
+let savedSwatches = [];
+
 const setup = () => {
     restoreLocalValues();
     changeColor();
@@ -15,7 +17,36 @@ const setup = () => {
 const removeSaved = (event) => {
     let savedSection = document.querySelector('#Saved');
     savedSection.removeChild(event.target.parentElement);
+
+    savedSwatches.length = 0;
+
+    let savedDivSwatches = document.querySelectorAll('.savedColors');
+    for(let i = 0; i < savedDivSwatches.length; i++){
+        let swatchValues = {};
+        let computedStyle = getComputedStyle(savedDivSwatches[i]).backgroundColor.toString();
+        let swatchColors = colorsFromComputedStyle(computedStyle);
+        swatchValues.rood= swatchColors[0];
+        swatchValues.groen = swatchColors[1];
+        swatchValues.blauw = swatchColors[2];
+
+        savedSwatches.push(swatchValues);
+    }
+
+    let savedSwatchesJSON = JSON.stringify(savedSwatches);
+    localStorage.setItem('Color.Picker.swatchRestore', savedSwatchesJSON);
+
     event.stopPropagation();
+}
+
+const colorsFromComputedStyle = (a) => {
+    let nakedValues = a.slice(4,-1);
+    let comma = nakedValues.indexOf(',');
+    let red = nakedValues.slice(0,comma).trim();
+    nakedValues = nakedValues.slice(comma + 1);
+    comma = nakedValues.indexOf(',');
+    let green = nakedValues.slice(0,comma).trim();
+    let blue = nakedValues.slice(comma + 1).trim();
+    return [red, green, blue];
 }
 
 const setToColor = (event) => {
@@ -24,42 +55,30 @@ const setToColor = (event) => {
     let greenSlider = document.querySelector('#GreenSlider');
     let blueSlider = document.querySelector('#BlueSlider');
 
-    let nakedValues = computedStyle.slice(4,-1);
-    let comma = nakedValues.indexOf(',');
-    let red = nakedValues.slice(0,comma).trim();
-    nakedValues = nakedValues.slice(comma + 1);
-    comma = nakedValues.indexOf(',');
-    let green = nakedValues.slice(0,comma).trim();
-    let blue = nakedValues.slice(comma + 1).trim();
+    let swatchColors = colorsFromComputedStyle(computedStyle);
 
-    redSlider.value = red;
-    greenSlider.value = green;
-    blueSlider.value = blue;
+    redSlider.value = swatchColors[0];
+    greenSlider.value = swatchColors[1];
+    blueSlider.value = swatchColors[2];
 
     changeColor();
 }
 
 const addToSaved = () =>{
-        let savedSection = document.querySelector('#Saved');
+        let swatchRestore = {};
 
         let red = document.querySelector('#RedSlider').value;
+        swatchRestore.rood = red;
         let green = document.querySelector('#GreenSlider').value;
+        swatchRestore.groen = green;
         let blue = document.querySelector('#BlueSlider').value;
+        swatchRestore.blauw = blue;
 
-        let div = document.createElement('div');
-        let input = document.createElement('input');
-        let label = document.createElement('label');
-        div.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-        div.classList.add('savedColors');
-        label.setAttribute('for', 'Deletebtn');
-        input.setAttribute('id', 'Deletebtn');
-        input.setAttribute('type', 'button');
-        input.setAttribute('value', 'X');
-        input.setAttribute('name', 'Delete');
-        input.addEventListener('click', removeSaved);
-        div.append(label);
-        div.append(input);
-        savedSection.append(div);
+        createSwatch(red, green, blue);
+
+        savedSwatches.push(swatchRestore);
+        let savedSwatchesJSON = JSON.stringify(savedSwatches);
+        localStorage.setItem('Color.Picker.swatchRestore', savedSwatchesJSON);
 }
 
 const changeColor = () => {
@@ -87,13 +106,46 @@ const changeColor = () => {
     swatch.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
+const createSwatch = (a,b,c) => {
+    let savedSection = document.querySelector('#Saved');
+
+    let div = document.createElement('div');
+    let input = document.createElement('input');
+    let label = document.createElement('label');
+    div.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+    div.classList.add('savedColors');
+    label.setAttribute('for', 'Deletebtn');
+    input.setAttribute('id', 'Deletebtn');
+    input.setAttribute('type', 'button');
+    input.setAttribute('value', 'X');
+    input.setAttribute('name', 'Delete');
+    input.addEventListener('click', removeSaved);
+    div.append(label);
+    div.append(input);
+    savedSection.append(div);
+}
+
 const restoreLocalValues = () => {
     if(localStorage.getItem('Color.Picker.sliderRestore')!==null){
-        let localStorageJSON = localStorage.getItem('Color.Picker.sliderRestore');
-        let localStorageObject = JSON.parse(localStorageJSON);
-        document.querySelector('#RedSlider').value = localStorageObject.rood;
-        document.querySelector('#BlueSlider').value = localStorageObject.blauw;
-        document.querySelector('#GreenSlider').value = localStorageObject.groen;
+        let localSliderStorageJSON = localStorage.getItem('Color.Picker.sliderRestore');
+        let localSliderStorageObject = JSON.parse(localSliderStorageJSON);
+        document.querySelector('#RedSlider').value = localSliderStorageObject.rood;
+        document.querySelector('#BlueSlider').value = localSliderStorageObject.blauw;
+        document.querySelector('#GreenSlider').value = localSliderStorageObject.groen;
+    }
+    if(localStorage.getItem('Color.Picker.swatchRestore')!==null){
+        let localSwatchStorageJSON = localStorage.getItem('Color.Picker.swatchRestore');
+        let localSwatchStorageList = JSON.parse(localSwatchStorageJSON);
+        for(let i = 0; i < localSwatchStorageList.length; i++){
+            let swatchObject = localSwatchStorageList[i];
+            savedSwatches.push(swatchObject);
+
+            let red = swatchObject.rood;
+            let green = swatchObject.groen;
+            let blue = swatchObject.blauw;
+
+            createSwatch(red, green, blue);
+        }
     }
 }
 
